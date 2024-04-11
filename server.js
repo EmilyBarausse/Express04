b3c4860b5fb527c4292cbd90690d127d2d8983e8
EmilyBarausse <emilybarausse2003@gmail.com>
2024-04-11 14:53:24 -0300

    subir arquivos iniciais

---
+++ server.js
@@ -0,0 +1,18 @@
+//criar a infraestrutura básica
+
+const express = require('express');
+const mysql = require('mysql2');
+
+const mysql_config = require('./mysql_config');
+
+const app = express();
+
+app.listen(3000,()=>{
+    console.log("Servidor no ar")
+})
+
+const connection = mysql.createConnection(mysql_config);
+
+// app.get('/', (req, res)=>{
+
+// })
